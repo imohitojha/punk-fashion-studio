34dfea9f54cc622e348c37601c2f16c69ad424bc
Mohit Ojha <MOjha@disa.com>
2023-07-23 16:38:54 +0530

    fixed form button color

--- app/form/page.js
+++ app/form/page.js
@@ -126,7 +126,7 @@ export default function Form() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="bg-[#ff3d3e] rounded-full flex w-full justify-center rounded-md px-3 py-1.5 text-md font-semibold leading-6 shadow-sm hover:bg-[#d40f0f] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
                 >
                   Book now
                 </button>
