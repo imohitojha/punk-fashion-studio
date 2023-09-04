5f15c38f258e5f4a09d470ee0113b5d7dc6e3951
Mohit Ojha <MOjha@disa.com>
2023-09-05 00:41:24 +0530

    made footer responsive

--- components/Footer.js
+++ components/Footer.js
@@ -6,31 +6,31 @@ export default function Footer() {
   return (
     <div className="relative top-28">
     <hr className="m-auto h-1 bg-gray-50 border-0 w-11/12" />
-      <footer className="h-32 flex justify-center items-start m-20 font-oswald mb-72 mx-20">
+      <footer className="h-32 flex justify-center items-start m-20 font-oswald mb-72 lg:mx-20 mx-10">
         <div className="w-1/2">
           <div className="flex flex-col justify-center items-start">
-            <p className="text-6xl font-bold mb-16 uppercase text-[#ff3d3e] font-bebas-neue">
+            <p className="md:text-6xl text-3xl font-bold mb-16 uppercase text-[#ff3d3e] font-bebas-neue">
               The Punk Fashion Studio
             </p>
-            <div className="flex justify-start items-center">
+            <div className="flex justify-start items-center md:flex-row flex-col gap-y-4 gap-x-8">
               <Icon
-                className="text-[#ff3d3e] text-5xl"
+                className="text-[#ff3d3e] md:text-5xl text-4xl"
                 icon="akar-icons:instagram-fill"
               />
               <Icon
-                className="text-[#ff3d3e] text-5xl ml-8"
+                className="text-[#ff3d3e] md:text-5xl text-4xl"
                 icon="akar-icons:facebook-fill"
               />
               <Icon
-                className="text-[#ff3d3e] text-5xl ml-8"
+                className="text-[#ff3d3e] md:text-5xl text-4xl"
                 icon="akar-icons:twitter-fill"
               />
               <Icon
-                className="text-[#ff3d3e] text-5xl ml-8"
+                className="text-[#ff3d3e] md:text-5xl text-4xl"
                 icon="akar-icons:youtube-fill"
               />
               <Icon
-                className="text-[#ff3d3e] text-5xl ml-8"
+                className="text-[#ff3d3e] md:text-5xl text-4xl"
                 icon="akar-icons:pinterest-fill"
               />
             </div>
@@ -39,16 +39,16 @@ export default function Footer() {
         <div className="w-1/2">
           <div
             id="contact-us"
-            className="flex flex-col justify-center items-start pl-12"
+            className="flex flex-col justify-center items-start md:pl-12 pl-2 md:mt-0 mt-9"
           >
-            <p className="text-4xl mb-16 uppercase">Contact Us</p>
+            <p className="md:text-4xl text-2xl lg:mb-16 md:mb-8 mb-4 uppercase">Contact Us</p>
             <div className="font-jost">
-              <div className="text-xl flex tracking-widest mb-8">
+              <div className="flex tracking-widest mb-4">
                 <Icon
-                  className="text-[#ff3d3e] text-5xl mr-8"
+                  className="text-[#ff3d3e] md:text-6xl text-9xl md:ml-0 -ml-10 lg:mr-8 mr-4"
                   icon="mdi:address-marker"
                 />
-                <div>
+                <div className="lg:text-xl md:text-lg text-sm md:break-normal break-all">
                   <span>
                     118/164 (7), Gumti No.5, Kaushalpuri, Darshan Purwa, Kanpur,
                     Uttar Pradesh 208012
@@ -61,26 +61,26 @@ export default function Footer() {
                   </Link>
                 </div>
               </div>
-              <p className="text-xl flex tracking-widest mb-8">
+              <p className="flex tracking-widest mb-4">
                 <Icon
-                  className="text-[#ff3d3e] text-4xl mr-8"
+                  className="text-[#ff3d3e] md:text-4xl text-3xl md:ml-0 -ml-10 lg:mr-8 mr-4"
                   icon="mdi:phone"
                 />
-                +91 885855910, +91 8177006806
+                <span className="lg:text-xl md:text-lg text-sm">+91 8858559101,<br />+91 8177006806</span>
               </p>
-              <p className="text-xl flex tracking-widest mb-8">
+              <p className="flex tracking-widest mb-4">
                 <Icon
-                  className="text-[#ff3d3e] text-4xl mr-8"
+                  className="text-[#ff3d3e] md:text-4xl text-5xl md:ml-0 -ml-10 lg:mr-8 mr-4"
                   icon="mdi:email"
                 />
-                thepunkfashionstudio@gmail.com
+                <span className="lg:text-xl md:text-lg text-sm md:break-normal break-all">thepunkfashionstudio@gmail.com</span>
               </p>
             </div>
           </div>
         </div>
       </footer>
-      <div className="mb-8">
-        <p className="text-2xl text-center font-jost">
+      <div className="pb-8">
+        <p className="md:text-2xl text-lg text-center font-jost">
           &copy; {new Date().getFullYear()} The Punk Fashion Studio
         </p>
       </div>
